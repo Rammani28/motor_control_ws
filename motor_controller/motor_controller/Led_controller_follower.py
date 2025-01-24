c1a5f8b4600a1d24afeb91ea13dde82dd515a690
rammani <rammani@pi.com>
2025-01-24 09:40:29 +0000

    motor driver integration for single motor

--- motor_controller/motor_controller/Led_controller_follower.py
+++ motor_controller/motor_controller/Led_controller_follower.py
@@ -6,19 +6,33 @@ import RPi.GPIO as GPIO  # https://pypi.org/project/RPi.GPIO/
 class LedSubscriberNode(Node):
     def __init__(self):
         super().__init__('led_subscriber') # name of node
+        print("subscriber initialized")
         self.subscription = self.create_subscription(msg_type=String, topic='/led_control', callback=self.listener_callback, qos_profile=10) # subscribe topic /led_control using String type, queue size is 10
-        self.led_pin = 23 #BCM numbering
+        # self.led_pin = 23 #BCM numbering      
+        self.in1 = 23
+        self.in2 = 22
+
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.led_pin, GPIO.OUT)
+        # GPIO.setup(self.led_pin, GPIO.OUT)
+        GPIO.setup(self.in1, GPIO.OUT)
+        GPIO.setup(self.in2, GPIO.OUT)
 
     def listener_callback(self, msg):
         self.get_logger().info(f"Received: {msg.data}")
         if msg.data.lower() == 'on':
-            GPIO.output(self.led_pin, GPIO.HIGH)
-            self.get_logger().info("led turned on")
+            # GPIO.output(self.led_pin, GPIO.HIGH
+            GPIO.output(self.in1, GPIO.HIGH)
+            GPIO.output(self.in2, GPIO.LOW)
+            # self.get_logger().info("led turned on")
+            self.get_logger().info("motor rotating forward")
+
         elif msg.data.lower() == 'off':
-            GPIO.output(self.led_pin, GPIO.LOW)
-            self.get_logger().info("led turned off")
+            # GPIO.output(self.led_pin, GPIO.LOW)
+            GPIO.output(self.in1, GPIO.LOW)
+            GPIO.output(self.in2, GPIO.HIGH)
+            # self.get_logger().info("led turned off")
+            self.get_logger().info("lmotor rotating reverse")
+
         else:
             self.get_logger().info("invalid command")
         
