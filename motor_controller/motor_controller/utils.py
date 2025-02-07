9065a3a2a096d56238dd2d5d6929724a06fb3804
rammani <rammani@pi.com>
2025-02-07 06:41:30 +0000

    updated V

--- motor_controller/motor_controller/utils.py
+++ motor_controller/motor_controller/utils.py
@@ -3,16 +3,17 @@ from math import sin, cos
 PI = 3.14159265359
 R = 0.029 # meters
 D = 0.15 # meters
-V = 1 # say 1 m/s
+V = 0.911 # say v 300*2PI/60 = rad/sec, v=w/R, V = 300*2PI/60*R
 
 
 def target_wheel_rpm(theta:float, wbz:float) -> tuple:
     """return u1, u2, u3,(for linear movement) in rpm"""
     theta = PI/180 * theta # convert to radian
+    wbz = -PI if wbz == -1 else PI
     vbx = sin(theta) * V
     vby = cos(theta) * V
-    u1 = (-D*wbz + vbx) / R *( 60 / (2 * PI))# how fast wheel 1 must rotate
+    u1 = (-D*wbz + vbx) / R * ( 60 / (2 * PI))# how fast wheel 1 must rotate
     u2 = (-D*wbz - 0.5*vbx - 0.866*vby) / R * ( 60 / (2 * PI))
     u3 = (-D*wbz - 0.5*vbx + 0.866*vby) / R * ( 60 / (2 * PI))
-    return u1/2, u2/2, u3/2 # return the target pwm of each wheel 
+    return u1, u2, u3 # return the target rpm of each wheel 
 
